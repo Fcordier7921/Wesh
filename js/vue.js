a7c10adc3b836742c07c4c4eebfe7fa871580b96
Fcordier7921 <f.cordier@codeur.online>
2021-02-03 16:17:18 +0100

    debut des filtre fin

--- js/vue.js
+++ js/vue.js
@@ -1,8 +1,4 @@
- function UpdatePost(){//appelle de la base de donné
-                axios.get('https://fakestoreapi.com/products').then(reponse => this.posts= reponse.data).catch(erreur =>this.posts=[{title: "Erreur de chargement"}]);
-                localStorage.setItem("posts", JSON.stringify(this.posts));
 
-              }
 
 new Vue({
           el:'#app',
@@ -10,44 +6,84 @@ new Vue({
           post:'',
           posts: [],
           shop:[],
+          shopprice:'',
           selected:'',
           selectedp:'',
           selectedsearch:'',
           showModal: false
           },
-          computed:{
-              filteredlist(){
-                return this.posts.filter((post)=> {
-                  return post.description.toUpperCase().includes(this.selectedsearch.toUpperCase());
-                  // return post.title.toUpperCase().includes(this.selectedsearch.toUpperCase());
-                  // return post.category.includes(this.selected);
-                  // return post.price.includes(this.selected);
-
-                });
-              }
-          },
-          mounted(){
-            if(localStorage.getItem('posts')){
-            this.posts=JSON.parse(localStorage.getItem('posts'));
-            }else if(localStorage.getItem('shop')) {
-              this.posts=JSON.parse(localStorage.getItem('shop'));
-            }
-          },
           
-          methods:{
-            UpdatePost(){//appelle de la base de donné
-              
-
+          mounted(){
+            
+            axios.get('https://fakestoreapi.com/products').then(reponse => this.posts= reponse.data).catch(erreur =>this.posts=[{title: "Erreur de chargement"}]);
+                          
+            if(localStorage.getItem('shop'))
+            {
+      
+            this.shop=JSON.parse(localStorage.getItem('shop'));
             }
-            ,
-              Updaetshop(id){//ajouter au panier
-                this.shop.push(this.posts[id]);
+          },
+          computed:
+          {
+            filteredRecipes() 
+            {
+              let tempRecipes = this.posts
+              
+              // Process search input
+              if (this.selectedsearch != '' && this.selectedsearch) //barre de recherche
+              {
+                  tempRecipes = tempRecipes.filter((post) => 
+                  {
+                    return (post.title
+                      .toUpperCase()
+                      .includes(this.selectedsearch.toUpperCase())) || (post.description
+                        .toUpperCase()
+                        .includes(this.selectedsearch.toUpperCase()));
+                  })
+              }
+              if (this.selected != '' && this.selected) // selection par catégorie
+              {
+                  tempRecipes = tempRecipes.filter((post) => 
+                  {
+                    return post.category === this.selected;
+                  })
+              }
+              if (this.selectedp != '' && this.selectedp) //selection par prix
+              {
+                  tempRecipes = tempRecipes.filter((post) => 
+                  {
+                    if((this.selectedp) == 10 )
+                    {
+                      return post.price <=10;
+                    }
+                    if((this.selectedp) == 50 )
+                    {
+                      return (post.price >=10) && (post.price <=50)
+                    }
+                    if((this.selectedp) == 51 )
+                    {
+                      return (post.price >=50) && (post.price <=100)
+                    }
+                    if((this.selectedp) == 101 )
+                    {
+                      return post.price >100
+                    }
+                  })
+              }
+                return tempRecipes 
+                
+                
+            } 
+
+            
+          },
+          methods:
+          {
+            Updaetshop(id, price)
+              {//ajouter au panier
+                this.shop.push(this.posts[id])
                 localStorage.setItem("shop", JSON.stringify(this.shop));
               }
           },
-          mounted(){
-            axios.get('https://fakestoreapi.com/products').then(reponse => this.posts= reponse.data).catch(erreur =>this.posts=[{title: "Erreur de chargement"}]);
-                          localStorage.setItem("posts", JSON.stringify(this.posts));
-          }
+         
     })
-    
