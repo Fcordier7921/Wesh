6a1c274b5f32bdea28948a6f92b42ca612bc1180
Fcordier7921 <f.cordier@codeur.online>
2021-02-05 20:54:02 +0100

    avant bug

--- js/vue.js
+++ js/vue.js
@@ -4,25 +4,36 @@ new Vue({
           post:'',
           posts: [],
           shop:[],
+          detail:[],
+          detalbool:true,
+          detalindex: '',
           shopprice:'',
           selected:'',
           selectedp:'',
           selectedsearch:'',
-          showModal: false
+          showModal: false,
+          showModal2: false,
+          ht:0,
+          tva:0,
+          ttc:0
+          
           },
           
           mounted(){
             
             axios.get('https://fakestoreapi.com/products').then(reponse => this.posts= reponse.data).catch(erreur =>this.posts=[{title: "Erreur de chargement"}]);
-                          
+            
+
             if(localStorage.getItem('shop'))
             {
       
             this.shop=JSON.parse(localStorage.getItem('shop'));
             }
+            
           },
           computed:
           {
+            
             filteredRecipes() 
             {
               let tempRecipes = this.posts
@@ -64,19 +75,41 @@ new Vue({
                     }
                     if((this.selectedp) == 101 )
                     {
+                      localStorage.setItem("posts", JSON.stringify(this.posts));
                       return post.price >100
                     }
                   })
               }
-                return tempRecipes 
-                
-                
-            } 
+                return tempRecipes;
 
-            
+                
+                
+            }, 
+
+            Addprice(){
+
+                  let somme =0;
+                  for( let i=0; i< this.shop.length; i++){
+                    somme += (this.shop[i].price) * (this.shop[i].quantity)
+          
+                  this.ht = somme;
+                  return this.ht.toFixed(2);
+                
+                }
+              },
+              addtva(){
+
+                  this.tva= (this.ht * 0.2);
+                  return this.tva.toFixed(2)
+              },
+              addttc(){
+                
+                return (this.ht+ this.tva).toFixed(2);
+              }  
           },
           methods:
           {
+
             Updaetshop(id)
               {//ajouter au panier
                 this.shop.push({
@@ -87,9 +120,61 @@ new Vue({
                 })
                 localStorage.setItem("shop", JSON.stringify(this.shop));
               },
-            toggleModale(){
-              this.showModal = !this.showModal
+
+            toggleModale()
+              {
+                this.showModal = !this.showModal;
+              },
+              toggleModale2()
+              {
+                this.showModal2 = !this.showModal2;
+              },
+            dellShop(id)
+            {
+              this.shop.splice(id, 1);
+              if(this.shop.length <1){
+                this.ht =0;
+              }
+              localStorage.setItem("shop", JSON.stringify(this.shop));
+            },
+            addQuantity(id)
+            {
+              let allqt= this.shop[id].quantity;
+              let vueqt= allqt +1;
+              this.shop[id].quantity= vueqt
+              localStorage.setItem("shop", JSON.stringify(this.shop));              
+            },
+            dellQuantity(id)
+            {
+              if((this.shop[id].quantity) >1)
+              {
+              let allqt= this.shop[id].quantity;
+              let vueqt= allqt -1;
+              this.shop[id].quantity= vueqt
+              localStorage.setItem("shop", JSON.stringify(this.shop)); 
+              }else{
+                this.shop.splice(id, 1);
+                this.ht=0;
+                localStorage.setItem("shop", JSON.stringify(this.shop));
+              }           
+            },
+            deatailFuction(id){
+ 
+              this.detail.push({
+                title:this.posts[id].title,
+                image: this.posts[id].image,
+                price: this.posts[id].price,
+                quantity: 1,
+                description: this.posts[id].description
+              })
+              this.detalindex = id;
+              this.detalbool = !this.detalbool;
+
+            },
+            refrech(){
+              document.location.reload();
             }
+            
           }
          
     })
